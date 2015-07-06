95b8a89bcb13b4d1a8f3c09699b21f0371758e11
chrisr22 <Chris@HP-Chromebook.local>
2015-07-05 23:12:24 -0500

    Initial Commit

---
+++ Rabbit Food/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rabbit Food
+//
+//  Created by Chris on 7/5/15.
+//  Copyright (c) 2015 AloaLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
